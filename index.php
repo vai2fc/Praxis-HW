9926093b2a02097a3d1950970cec733446c3eb12
Veronica Ikeshoji-Orlati <vai2fc@virginia.edu>
2013-12-06 12:42:41 -0500

    school address added.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Praxis Homework</title>
+</head>
+
+<body>
+	<h1>School Address</h1>
+
+	<p>
+		<?php
+			$address1 = "McIntire Department of Art\nUniversity of Virginia\nCharlottesville, VA 22903";
+			echo "$address1";
+		?>
+	</p>
+</body>
+
+</html>
