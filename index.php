457e920632acf1552667d97a5766fb59bffffcc7
Veronica Ikeshoji-Orlati <vai2fc@virginia.edu>
2013-12-08 18:44:17 -0500

    added authors and death dates.

--- index.php
+++ index.php
@@ -9,12 +9,24 @@
 <body>
 	<h1>School Address</h1>
 
-	<p>
-		<?php
-			$address1 = "McIntire Department of Art\nUniversity of Virginia\nCharlottesville, VA 22903";
-			echo $address1;
-		?>
-	</p>
+	<?php
+		$address1 = "<p>McIntire Department of Art<br/>University of Virginia<br/>Charlottesville, VA 22903</p>";
+		echo $address1;
+	?>
+
+	<h1>Dead People</h1>
+
+	<?php
+		$authors = array(
+			"Charles Dickens" => "1870",
+			"William Thackeray" => "1863",
+			"Anthony Trollope" => "1882",
+			"Gerard Manley Hopkins" => "1889");
+		foreach($authors as $name => $date) {
+			echo "<ul>$name died in $date.</ul>";
+		}
+	?>
+
 </body>
 
 </html>
